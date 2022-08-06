7c9e9092fc3b6e873d855717c51cac8359c673d6
Felipe-Noguez <felipenoguez@outlook.com.br>
2022-08-05 22:29:35 -0300

    DIO desafio banco

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente felipe = new Cliente();
+		felipe.setNome("Felipe Noguez");
+		
+		Conta cc = new ContaCorrente(felipe);
+		Conta poupanca = new ContaPoupanca(felipe);
+		
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
